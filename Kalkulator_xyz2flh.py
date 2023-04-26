adcfd1b370099664eef36dd8e10df5a391c377fd
Adrian Maksymiuk <adiri12613@gmail.com>
2023-04-26 12:54:52 +0200

    połączenie kalkulatora xyz2flh i xyz2PL1992_PL2000

--- Kalkulator_xyz2flh.py
+++ Kalkulator_xyz2flh.py
@@ -33,6 +33,22 @@ print("")
 print("")
 print("")
 print("")
+fi, lam, ha = geo.hirvonen(args.x, args.y, args.z)
+if lam >= 13.5 and lam <= 25.5 and fi <= 55.0 and fi >= 48.9:
+    x92, y92 = geo.flh2PL92(args.f,args.l)
+    x00, y00 = geo.flh2PL00(args.f,args.l)
+    print(f"Wyniki_z_transformacji_flh2PL92_I_flh2PL00; X1992 = {x92:^.3f}[m], Y1992 = {y92:^.3f}[m], X2000 = {x00:^.3f}[m], Y2000 = {y00:^.3f}[m]")
+else:
+    x92 = " '-' " 
+    y92 = " '-' " 
+    x00 = " '-' " 
+    y00 = " '-' " 
+    print(f"Wyniki_z_transformacji_flh2PL92_i_flh2PL00; X1992 = {x92}[m], Y1992 = {y92}[m], X2000 = {x00}[m], Y2000 = {y00}[m]")
+    print("to położenie nie jest obsługiwane przez nie jest obsługiwany przez układ współrzędnych płaskich PL1992 i PL2000")
+
+print("")
+print("Nazwa pliku głównego:", Transformacje_Projekt.__name__)
+
 
 """
 x92, y92 = geo.flh2PL92(f, l)
